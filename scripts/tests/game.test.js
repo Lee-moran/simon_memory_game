2b47b069c4ca906661381f6da333055d5942954f
Lee-moran <leemoran06@hotmail.com>
2022-05-09 14:59:06 +0000

    9 tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,8 +2,7 @@
  * @jest-environment jsdom
  */
 
- const { game } = require("../game");
-
+ const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
 
  beforeAll(() => {
      let fs = require("fs");
@@ -31,3 +30,24 @@
      });
  });
  
+ describe("newGame works correctly", () => {
+     beforeAll(() => {
+         game.score = 42;
+         game.playerMoves = ["button1", "button2"];
+         game.currentGame = ["button1", "button2"];
+         document.getElementById("score").innerText = "42";
+         newGame();
+     });
+     test("should set game score to zero", () => {
+         expect(game.score).toEqual(0);
+     });
+     test("should display 0 for the element with id of score", () => {
+         expect(document.getElementById("score").innerText).toEqual(0);
+     });
+     test("should clear the player moves array", () => {
+         expect(game.playerMoves.length).toBe(0);
+     });
+     test("should add one move to the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
+    });
+ });
